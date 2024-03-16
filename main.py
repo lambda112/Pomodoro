a7e76e87c47023626b75b1fe240c328bab0ecb6d
Kamran Azim <Kamazim121212@gmail.com>
2024-03-16 16:47:07 +0000

    Created window and created some constants

---
+++ main.py
@@ -0,0 +1,32 @@
+import tkinter
+
+# CONSTANTS
+PINK = "#e2929c"
+RED = "#e7305b"
+GREEN = "9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# UI SETUP
+window = tkinter.Tk()
+window.title("Pomodoro")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
