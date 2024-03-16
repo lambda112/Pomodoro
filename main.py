1dceaed81602db204f790949c0b92e613fb063a5
Kamran Azim <Kamazim121212@gmail.com>
2024-03-16 19:15:21 +0000

    Created Start of user interface with image

--- main.py
+++ main.py
@@ -13,12 +13,18 @@ LONG_BREAK_MIN = 20
 # UI SETUP
 window = tkinter.Tk()
 window.title("Pomodoro")
+window.config(padx=100, pady=50, bg = YELLOW)
+
+# Canvas
+canvas = tkinter.Canvas(width=256, height=256, bg = YELLOW, highlightthickness=0)
+tomato_img = tkinter.PhotoImage(file = "tomato.png")
+canvas.create_image(128, 118, image = tomato_img)
+canvas.create_text(128, 148, text = "00:00", fill = "white", font=(FONT_NAME, 30, "bold"))
+canvas.pack()
 
 
 
-
-
-
+5
 
 
 
