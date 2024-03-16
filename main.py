dfc72e76af8155055104beee055668179ac1ebd2
Kamran Azim <Kamazim121212@gmail.com>
2024-03-16 21:33:48 +0000

    Created UI with image and two buttons

--- main.py
+++ main.py
@@ -15,16 +15,26 @@ window = tkinter.Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg = YELLOW)
 
+label_title = tkinter.Label(text = "Timer", font = (FONT_NAME, 40), fg = "red", bg = YELLOW)
+label_title.grid(column=1, row=0)
+
+check = "✓"
+label_check = tkinter.Label(text=check, font = (FONT_NAME, 15), bg = YELLOW, fg = RED)
+label_check.grid(column=1, row = 3)
+
+start_button = tkinter.Button(text = "Start", font = (FONT_NAME, 10, "bold"))
+start_button.grid(column=0, row=2)
+
+reset_button = tkinter.Button(text = "Reset", font = (FONT_NAME, 10, "bold"))
+reset_button.grid(column=2, row=2)
+
 # Canvas
 canvas = tkinter.Canvas(width=256, height=256, bg = YELLOW, highlightthickness=0)
 tomato_img = tkinter.PhotoImage(file = "tomato.png")
-canvas.create_image(128, 118, image = tomato_img)
-canvas.create_text(128, 148, text = "00:00", fill = "white", font=(FONT_NAME, 30, "bold"))
-canvas.pack()
+tomato = canvas.create_image(128, 118, image = tomato_img)
+timer = canvas.create_text(128, 148, text = "00:00", fill = "white", font=(FONT_NAME, 30, "bold"))
+canvas.grid(column=1,row=1)
 
-
-
-5
 
 
 
